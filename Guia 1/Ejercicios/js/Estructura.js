57195f940bd46ca73373bcc096c943d068ce0983
DiegoAxe <100392580+DiegoAxe@users.noreply.github.com>
2022-07-08 09:47:27 -0600

    Upload: Estructura JS

---
+++ Guia 1/Ejercicios/js/Estructura.js
@@ -0,0 +1,2 @@
+document.write("<header> Header </header> <nav> Navigation </nav> <section> <article id='art1'> Article </article>" + 
+"<article id='art2'> Article </article> <article id='art3'> Article </article> </section> <footer> Footer </footer>");
